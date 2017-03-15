84127d9c67d2771d96d183272b55c4da18238d37
Administrator <jack_ruan@aliyun.com>
2017-03-15 19:35:08 +0800

    Test

--- app/src/main/java/www/formssi/goodtaste/activity/MainActivity.java
+++ app/src/main/java/www/formssi/goodtaste/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package www.formssi.goodtaste;
+package www.formssi.goodtaste.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import www.formssi.goodtaste.R;
 
 public class MainActivity extends AppCompatActivity {
 
