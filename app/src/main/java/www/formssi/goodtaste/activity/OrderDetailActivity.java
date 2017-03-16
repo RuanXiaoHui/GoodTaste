a7a0065f23a4324bdd68e4e4eb43778283ccd54b
qq724418408 <724418408@qq.com>
2017-03-16 19:41:28 +0800

    1.补充OrderBean

--- app/src/main/java/www/formssi/goodtaste/activity/OrderDetailActivity.java
+++ app/src/main/java/www/formssi/goodtaste/activity/OrderDetailActivity.java
@@ -1,19 +1,96 @@
 package www.formssi.goodtaste.activity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.List;
+
+import www.formssi.goodtaste.R;
 import www.formssi.goodtaste.activity.base.BaseActivity;
+import www.formssi.goodtaste.bean.FoodBean;
 
 /**
- * Created by Administrator on 2017/3/15.
- * 邮箱：724418408@qq.com
- * project: GoodTaste
+ * 订单详情Activity类
+ * 说明：订单详情页面信息显示
+ *
+ * @author qq724418408
  */
-public class OrderDetailActivity extends BaseActivity {
+public class OrderDetailActivity extends BaseActivity implements View.OnClickListener {
+
+    private TextView tvOrderStatus; // 订单状态
+    private Button btnOK; // 确认按钮：根据状态显示不同文字：去支付、评价、再来一单
+    private Button btnCancel; // 取消按钮：取消订单
+    private Button btnContactBusiness; // 联系商家按钮：拨打商家电话
+    private List<FoodBean> listFoodBean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_order_detail);
+        setContentView(R.layout.activity_order_detail);
+        initViews(); // 初始化控件
+        initEvents(); // 注册事件
+    }
+
+    private void initEvents() {
+
+    }
+
+    /**
+     * 初始化控件
+     */
+    public void initViews() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    class FoodListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return listFoodBean.size();
+        }
+
+        @Override
+        public FoodBean getItem(int position) {
+            return listFoodBean.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            FoodViewHolder holder = null;
+            if(convertView == null){
+                holder = new FoodViewHolder();
+                convertView = LayoutInflater.from(OrderDetailActivity.this).inflate(R.layout.item_order_food_list_viewl, null);
+                holder.tvFoodName = (TextView) convertView.findViewById(R.id.iv_order_item_food_name);
+                holder.tvFoodCount = (TextView) convertView.findViewById(R.id.iv_order_item_food_count);
+                holder.tvFoodPrice = (TextView) convertView.findViewById(R.id.iv_order_item_food_price);
+                convertView.setTag(holder);
+            } else {
+                holder = (FoodViewHolder) convertView.getTag();
+            }
+            FoodBean bean = getItem(position);
+            //holder.tvFoodName.setText();
+            return convertView;
+        }
+    }
+
+    class FoodViewHolder {
+        TextView tvFoodName; // 食品名称
+        TextView tvFoodCount; // 食品数量
+        TextView tvFoodPrice; // 食品单价
     }
 }
