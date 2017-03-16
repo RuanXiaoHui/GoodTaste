9a447a3ad8db7d60635ab29502cd183f4dc73cfc
qq724418408 <724418408@qq.com>
2017-03-16 15:00:37 +0800

    订单详情页布局，commit by qq724418408

--- app/src/main/java/www/formssi/goodtaste/activity/OrderDetailActivity.java
+++ app/src/main/java/www/formssi/goodtaste/activity/OrderDetailActivity.java
@@ -1,20 +1,19 @@
 package www.formssi.goodtaste.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import www.formssi.goodtaste.R;
+import www.formssi.goodtaste.activity.base.BaseActivity;
 
 /**
  * Created by Administrator on 2017/3/15.
  * 邮箱：724418408@qq.com
  * project: GoodTaste
  */
-public class OrderDetailActivity extends AppCompatActivity {
+public class OrderDetailActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_detail);
+        //setContentView(R.layout.activity_order_detail);
     }
 }
