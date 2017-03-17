aa13aa74558f345fc19e3e72fc305bca5e42b7ba
Amy011 <1838263946@qq.com>
2017-03-17 10:07:07 +0800

    我的界面

--- app/src/main/java/www/formssi/goodtaste/activity/PersonalActivity.java
+++ app/src/main/java/www/formssi/goodtaste/activity/PersonalActivity.java
@@ -1,15 +1,15 @@
 package www.formssi.goodtaste.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import www.formssi.goodtaste.R;
+import www.formssi.goodtaste.activity.base.BaseActivity;
 
-public class UserCenterActivity extends AppCompatActivity {
+public class PersonalActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_center);
+        setContentView(R.layout.activity_personal);
     }
 }
