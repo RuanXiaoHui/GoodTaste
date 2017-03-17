55fb2983acd70aa015c29d9216722e26ba3c7c60
HighSky50GT <1043073802@qq.com>
2017-03-17 14:19:17 +0800

    增加orderfragment没有订单时点击去下单按钮跳转到首页

--- app/src/main/java/www/formssi/goodtaste/fragment/OrderFragment.java
+++ app/src/main/java/www/formssi/goodtaste/fragment/OrderFragment.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,17 +19,22 @@ import www.formssi.goodtaste.R;
 import www.formssi.goodtaste.adapter.OrderAdapter;
 import www.formssi.goodtaste.bean.OrderBean;
 
-public class OrderFragment extends Fragment {
+public class OrderFragment extends Fragment implements View.OnClickListener {
 
     RecyclerView rvOrderList;
     List<OrderBean> orders;
     OrderAdapter orderAdapter;
+    LinearLayout lltNoOrder;
+    Button btnGoSingle;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_order, null);
         rvOrderList = (RecyclerView) v.findViewById(R.id.rvOrderList);
+        lltNoOrder = (LinearLayout) v.findViewById(R.id.lltNoOrder);
+        btnGoSingle = (Button) v.findViewById(R.id.btnGoSingle);
+        btnGoSingle.setOnClickListener(this);
 
         orders = new ArrayList<>();
 
@@ -40,11 +48,30 @@ public class OrderFragment extends Fragment {
 //        orders.add(new OrderBean("好味道","2017-03-11 22:30","宫保鸡丁","23","交易完成"));
 //        orders.add(new OrderBean("好味道","2017-03-11 22:30","宫保鸡丁","23","交易完成"));
 
-        orderAdapter = new OrderAdapter(orders,getContext());
+        orderAdapter = new OrderAdapter(orders, getContext());
         rvOrderList.setLayoutManager(new LinearLayoutManager(getContext()));
         rvOrderList.setAdapter(orderAdapter);
 
-
+        if (orders.size() == 0) {
+            rvOrderList.setVisibility(View.GONE);
+            lltNoOrder.setVisibility(View.VISIBLE);
+        }
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnGoSingle:
+                if (getActivity() instanceof MeOnClickListener) {
+                    ((MeOnClickListener) getActivity()).onBtnGoSingleClick();
+                }
+                break;
+        }
+    }
+
+    public interface MeOnClickListener {
+        void onBtnGoSingleClick();
+
     }
 }
