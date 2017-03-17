fd34c337bdf02a78e5dbe667306aaa0c187e2d70
RuanXiaoHui <jack__ruan@aliyun.com>
2017-03-17 16:00:45 +0800

    商店详情activity

--- app/src/main/java/www/formssi/goodtaste/activity/GoodsDetailActivity.java
+++ app/src/main/java/www/formssi/goodtaste/activity/GoodsDetailActivity.java
@@ -14,24 +14,35 @@ import www.formssi.goodtaste.activity.base.BaseActivity;
 
 public class GoodsDetailActivity extends BaseActivity {
     private ListView lvLeftMenu;
+    private ListView lvRightFoods;
     private List<Map<String,String>> mLeftData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_detail);
-        lvLeftMenu= (ListView) findViewById(R.id.lvLeftMenu);
+        initView();
+        initData();
+        initListener();
 
+    }
+
+    private void initView() {
+        lvLeftMenu= (ListView) findViewById(R.id.lvLeftMenu);
+        lvRightFoods= (ListView) findViewById(R.id.lvRightFoods);
+    }
+
+    private void initData() {
         mLeftData=new ArrayList<>();
         for (int i = 0; i <17 ; i++) {
             Map<String,String> map=new HashMap<>();
             map.put("menu","菜单"+i);
             mLeftData.add(map);
         }
-
+    }
+    private void initListener() {
         SimpleAdapter sim=new SimpleAdapter(this,mLeftData,R.layout.item_goods_leftmenu,new String[]{"menu"},new int[]{R.id.tv});
         lvLeftMenu.setAdapter(sim);
-
-
     }
 }
