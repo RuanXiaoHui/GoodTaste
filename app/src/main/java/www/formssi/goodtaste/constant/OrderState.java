ec2138eda4851fb24d17fb77261e17c9d631b179
HighSky50GT <1043073802@qq.com>
2017-03-18 13:07:51 +0800

    增加订单状态常量类

---
+++ app/src/main/java/www/formssi/goodtaste/constant/OrderState.java
@@ -0,0 +1,32 @@
+package www.formssi.goodtaste.constant;
+
+/**
+ * Created by GTs on 2017-03-18.
+ * 订单的状态
+ */
+
+public class OrderState {
+    /**
+     * 未付款
+     */
+    public static final int NOT_PAY = 1;
+    /**
+     * 未发货
+     */
+    public static final int NOT_DELIVERY = 2;
+    /**
+     * 送餐中
+     */
+    public static final int DELIVERY_ING = 3;
+    /**
+     * 未评论
+     */
+    public static final int NOT_COMMENT = 4;
+    /**
+     * 已完成
+     */
+    public static final int FINISH = 5;
+
+
+
+}
