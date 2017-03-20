84c27492a1d346ffd1df2bdcbae0d99b62f24a6e
Administrator <jack_ruan@aliyun.com>
2017-03-20 23:23:52 +0800

    1.自定义ScrollView 2.实现商店详情页面滑动顶部栏渐变效果

--- app/src/main/java/www/formssi/goodtaste/activity/GoodsDetailActivity.java
+++ app/src/main/java/www/formssi/goodtaste/activity/GoodsDetailActivity.java
@@ -1,12 +1,17 @@
 package www.formssi.goodtaste.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.io.Serializable;
@@ -21,8 +26,12 @@ import www.formssi.goodtaste.adapter.ShopMenuAdapter;
 import www.formssi.goodtaste.bean.FoodBean;
 import www.formssi.goodtaste.bean.GoodsMenu;
 import www.formssi.goodtaste.bean.ShopBean;
+import www.formssi.goodtaste.widget.CustomScrollView;
 
-public class GoodsDetailActivity extends BaseActivity {
+public class GoodsDetailActivity extends BaseActivity implements CustomScrollView.ScrollViewListener {
+
+    private CustomScrollView scLayoutView;
+    private RelativeLayout rlView;
     private ListView lvLeftMenu;
     private ListView lvRightFoods;
     private ShopBean mShopBean;
@@ -30,6 +39,9 @@ public class GoodsDetailActivity extends BaseActivity {
     private List<FoodBean> mFoodBean;
     private List<FoodBean> mRefreshBean;
     private ImageView iv_backTitlebar_back;
+    private RelativeLayout rlToobar;
+    private TextView tv_backTitlebar_title;
+
     private ImageView ivShopimg;               //商店图片
     private TextView ivShopTime;              //配送时间
     private TextView ivShopDesc;              //店铺描述
@@ -41,6 +53,7 @@ public class GoodsDetailActivity extends BaseActivity {
     private ShopDataAdapter Adapter;
     private List<FoodBean> mFoodbConfirm;
     private int Money=0;
+    private int mHeight=0;
 
 
     @Override
@@ -48,12 +61,18 @@ public class GoodsDetailActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_detail);
         initView();
+        initScrollView();
         initData();
         initListener();
 
     }
 
     private void initView() {
+        scLayoutView= (CustomScrollView) findViewById(R.id.scLayoutView);
+        rlView= (RelativeLayout) findViewById(R.id.rlView);
+        rlToobar= (RelativeLayout) findViewById(R.id.rlToobar);
+        tv_backTitlebar_title= (TextView) findViewById(R.id.tv_backTitlebar_title);
+
         lvLeftMenu= (ListView) findViewById(R.id.lvLeftMenu);
         lvRightFoods= (ListView) findViewById(R.id.lvRightFoods);
         ivShopimg= (ImageView) findViewById(R.id.ivShopimg);
@@ -64,8 +83,9 @@ public class GoodsDetailActivity extends BaseActivity {
         tvGoodsMoney= (TextView) findViewById(R.id.tvGoodsMoney);
         tvShopMoney= (TextView) findViewById(R.id.tvShopMoney);
         btnSubmintOrder= (Button) findViewById(R.id.btnSubmintOrder);
-        tv_backTitlebar_center_title= (TextView) findViewById(R.id.tv_backTitlebar_center_title);
         mFoodbConfirm=new ArrayList<>();
+        // lvRightFoods.setFocusable(false);
+        scLayoutView.smoothScrollTo(0, 0);
         btnSubmintOrder.setEnabled(false);
     }
 
@@ -86,7 +106,6 @@ public class GoodsDetailActivity extends BaseActivity {
 
     }
     private void initListener() {
-        tv_backTitlebar_center_title.setText(mShopBean.getShopName());
         ivShopTime.setText("平均配送时间:"+mShopBean.getShopBusinessHours());
         ivShopDesc.setText("店家描述："+mShopBean.getShopDesc());
         ivShopBusinessHours.setText("营业时间："+mShopBean.getShopBusinessHours());
@@ -94,6 +113,7 @@ public class GoodsDetailActivity extends BaseActivity {
         lvLeftMenu.setAdapter(new ShopMenuAdapter(mLeftMenu,this));
         Adapter= new ShopDataAdapter(mRefreshBean,this);
         lvRightFoods.setAdapter(Adapter);
+        setListViewHeightBasedOnChildren(lvRightFoods);
 
         lvLeftMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -148,6 +168,73 @@ public class GoodsDetailActivity extends BaseActivity {
                 finish();
             }
         });
+
+
+        scLayoutView.setOnScrollView(new CustomScrollView.ScrollViewListener() {
+            @Override
+            public void OnScrollViewChangeListener(int x, int y, int oldx, int oldy) {
+
+            }
+        });
+
+    }
+
+
+    //初始化ScrollView，并测量顶部背景图高度
+    private void initScrollView() {
+        ViewTreeObserver vto = rlView.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                rlView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                mHeight=rlView.getHeight();
+                scLayoutView.setOnScrollView(GoodsDetailActivity.this);
+            }
+        });
+
+    }
+
+
+    public void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+        int totalHeight = 0;
+        for (int i = 0, len = listAdapter.getCount(); i < len; i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight+ (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+    }
+
+
+    @Override
+    public void OnScrollViewChangeListener(int x, int y, int oldx, int oldy) {
+
+        if (y<=0){
+            rlToobar.setBackgroundColor(Color.argb(120,0, 149, 254));
+            tv_backTitlebar_title.setVisibility(View.GONE);
+        }else if(y>0&&y<=mHeight/3){
+            tv_backTitlebar_title.setVisibility(View.VISIBLE);
+            tv_backTitlebar_title.setTextColor(Color.argb(100,255,255,255));
+            tv_backTitlebar_title.setText(mShopBean.getShopName());
+            rlToobar.setBackgroundColor(Color.argb(120,0, 149, 254));
+
+        }else if (y>mHeight/3&&y<mHeight){
+
+            double bf=((y*1.0)/mHeight);
+            double alpha=(bf*255);
+            tv_backTitlebar_title.setVisibility(View.VISIBLE);
+            tv_backTitlebar_title.setTextColor(Color.argb((int)alpha,255,255,255));
+            tv_backTitlebar_title.setText(mShopBean.getShopName());
+            rlToobar.setBackgroundColor(Color.argb((int)alpha,0, 149, 254));
+
+        }
 
     }
 }
