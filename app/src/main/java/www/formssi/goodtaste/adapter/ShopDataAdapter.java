c44607338b2644c05174ced576c4c10969b4a9c7
RuanXiaoHui <jack__ruan@aliyun.com>
2017-03-20 15:10:38 +0800

    GoodsDetailActivity的更新

--- app/src/main/java/www/formssi/goodtaste/adapter/ShopDataAdapter.java
+++ app/src/main/java/www/formssi/goodtaste/adapter/ShopDataAdapter.java
@@ -8,7 +8,9 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import www.formssi.goodtaste.R;
 import www.formssi.goodtaste.bean.FoodBean;
@@ -23,15 +25,17 @@ public class ShopDataAdapter extends BaseAdapter {
     private List<FoodBean> mdatas;
     private LayoutInflater mInflater;
     private int CountNumber=0;
+    private Map<String,FoodBean> mBeans;
 
     public ShopDataAdapter(List<FoodBean> mdatas, Context context) {
         this.mdatas = mdatas;
         this.mInflater=LayoutInflater.from(context);
+        mBeans=new HashMap<String,FoodBean>();
     }
 
     //////////////*对外提供接口//////////////
     public interface  OnExtralClickListener{
-        void onClickMoney(int vue);
+        void onClickMoney(int vue,Map<String,FoodBean> beans);
     }
     public OnExtralClickListener onExtralClickListener;
 
@@ -73,33 +77,37 @@ public class ShopDataAdapter extends BaseAdapter {
         holder.ivFoodIcon.setImageResource(bean.getGoodsIcon());
         holder.tvFoodName.setText(bean.getGoodsName());
         holder.tvFoodBuyNumber.setText("月销量："+bean.getGoodsNumber()+"份");
-       holder.numberSubAdd.setNubNumBerSubAddClick(new NumberSubAdd.NumBerSubAddClick() {
-           @Override
-           public void AddBtnOnClick(View v, int vue) {
-              mdatas.get(i).setGoodsBuynumber(vue);
-               if (onExtralClickListener!=null){
-                   mdatas.get(i).setGoodsBuynumber(vue);
-                   int price= mdatas.get(i).getGoodsBuynumber();//获得当前的选项的单价
-                   CountNumber=CountNumber+price;
-                   onExtralClickListener.onClickMoney(CountNumber);
-               }
+        holder.tvFoodMoney.setText("￥"+bean.getGoodsMoney()+"元");
+        holder.numberSubAdd.setNubNumBerSubAddClick(new NumberSubAdd.NumBerSubAddClick() {
+            @Override
+            public void AddBtnOnClick(View v, int vue) {
+                if (onExtralClickListener!=null&&mdatas.get(i).getGoodsBuynumber()<=9){
+                    mdatas.get(i).setGoodsBuynumber(vue);
+                    int price= Integer.parseInt(mdatas.get(i).getGoodsMoney());//获得当前的选项的单价
+                    CountNumber=CountNumber+price;
+                    System.out.print(CountNumber);
 
-           }
+                    mBeans.put(mdatas.get(i).getGoodsId(),mdatas.get(i));
 
-           @Override
-           public void SubBtnOnclick(View v, int vue) {
-              // mdatas.get(i).setGoodsBuynumber(vue);
-               if (onExtralClickListener!=null&&vue>=1&&mdatas.get(i).getGoodsBuynumber()>1){
-                   int price= Integer.parseInt(mdatas.get(i).getGoodsMoney());//获得当前的选项的单价
-                   mdatas.get(i).setGoodsBuynumber(vue);
-                   CountNumber=CountNumber-price;
-                   onExtralClickListener.onClickMoney(CountNumber);
-               }else if(onExtralClickListener!=null&&vue>=1&&mdatas.get(i).getGoodsBuynumber()>1){
-                   mdatas.get(i).setGoodsBuynumber(vue);
-               }
+                    onExtralClickListener.onClickMoney(CountNumber,mBeans);
 
-           }
-       });
+                }
+            }
+
+            @Override
+            public void SubBtnOnclick(View v, int vue) {
+                if (onExtralClickListener!=null&&mdatas.get(i).getGoodsBuynumber()>=1){
+                    int price= Integer.parseInt(mdatas.get(i).getGoodsMoney());//获得当前的选项的单价
+                    mdatas.get(i).setGoodsBuynumber(vue);
+                    CountNumber=CountNumber-price;
+
+                    mBeans.put(mdatas.get(i).getGoodsId(),mdatas.get(i));
+
+                    onExtralClickListener.onClickMoney(CountNumber,mBeans);
+                }
+
+            }
+        });
         holder.numberSubAdd.setVue(bean.getGoodsBuynumber());
         return view;
     }
@@ -110,5 +118,11 @@ public class ShopDataAdapter extends BaseAdapter {
         TextView tvFoodBuyNumber;
         TextView tvFoodMoney;
         NumberSubAdd numberSubAdd;
+    }
+
+    public void addData(List<FoodBean> data){
+        mdatas.clear();
+        mdatas.addAll(data);
+        this.notifyDataSetChanged();
     }
 }
